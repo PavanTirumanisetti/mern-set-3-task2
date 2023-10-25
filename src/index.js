f96659aa792ba54e524bde10aa390640e7901d6f
PavanTirumanisetti <pavantirumanisetti@gmail.com>
2023-10-25 11:43:24 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calculator from './Components/Calculator';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Calculator></Calculator>
   </React.StrictMode>
 );
 
